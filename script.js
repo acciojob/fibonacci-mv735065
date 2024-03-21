5f69b9658874377319f23be6addcdd83e3197bb6
Vamshi Reddy <106398124+mv735065@users.noreply.github.com>
2024-03-21 07:21:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-function fibonacci(num) {
+function fibonacci(n) {
 // your code here
+ if(n==0 || n==1) return 0
+	if(n==2) return 1
+
+	return fibonacci(n-1)+fibonacci(n-2)
+
+	
 }
 
 module.exports = fibonacci;
